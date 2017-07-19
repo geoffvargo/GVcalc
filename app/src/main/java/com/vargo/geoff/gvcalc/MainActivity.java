0227bdb6d60cf27842c0479e442db802046daf7c
geoff <geoff.vargo@stonybrook.edu>
2017-07-19 01:47:24 -0400

    Started button implementation.

--- app/src/main/java/com/vargo/geoff/gvcalc/MainActivity.java
+++ app/src/main/java/com/vargo/geoff/gvcalc/MainActivity.java
@@ -2,15 +2,64 @@ package com.vargo.geoff.gvcalc;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import java.util.Stack;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+	protected static Stack<Double> numStack = new Stack<>();
+	protected static Stack<Character> opStack = new Stack<>();
+
+	protected String tempStr = "";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_main);
+	}
+
+	public void numBtnHandler(View v, char num) {
+
+	}
+
+	@Override
+	public void onClick(View view) {
+		switch (view.getId()) {
+			case R.id.zeroBTN:
+				tempStr = tempStr.concat("0");
+				((TextView)findViewById(R.id.dispTXT)).setText(tempStr);
+				break;
+			case R.id.oneBTN:
+				tempStr = tempStr.concat("1");
+				break;
+			case R.id.twoBTN:
+				tempStr = tempStr.concat("2");
+				break;
+			case R.id.threeBTN:
+				tempStr = tempStr.concat("3");
+				break;
+			case R.id.fourBTN:
+				tempStr = tempStr.concat("4");
+				break;
+			case R.id.fiveBTN:
+				tempStr = tempStr.concat("5");
+				break;
+			case R.id.sixBTN:
+				tempStr = tempStr.concat("6");
+				break;
+			case R.id.sevenBTN:
+				tempStr = tempStr.concat("7");
+				break;
+			case R.id.eightBTN:
+				tempStr = tempStr.concat("8");
+				break;
+			case R.id.nineBTN:
+				tempStr = tempStr.concat("9");
+				break;
+		}
 	}
 }
