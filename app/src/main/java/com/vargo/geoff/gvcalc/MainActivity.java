9dc5d0f256e234f478540ab058b00a4eac9d9e13
geoff <geoff.vargo@stonybrook.edu>
2017-08-04 16:07:02 -0400

    Added Token class to handle tokens.

--- app/src/main/java/com/vargo/geoff/gvcalc/MainActivity.java
+++ app/src/main/java/com/vargo/geoff/gvcalc/MainActivity.java
@@ -9,6 +9,12 @@ import android.widget.TextView;
 
 import java.util.Stack;
 
+import static com.vargo.geoff.gvcalc.MainActivity.Type.LEFT_PAREN;
+import static com.vargo.geoff.gvcalc.MainActivity.Type.NUM;
+import static com.vargo.geoff.gvcalc.MainActivity.Type.OP;
+import static com.vargo.geoff.gvcalc.MainActivity.Type.RIGHT_PAREN;
+import static com.vargo.geoff.gvcalc.MainActivity.Type.VAR;
+
 public class MainActivity extends Activity {
 
 	protected static Stack<Double> numStack = new Stack<>();
@@ -72,10 +78,76 @@ public class MainActivity extends Activity {
 		if (!tempStr.isEmpty()) {
 			numStack.push(Double.valueOf(tempStr));
 			tempStr = "";
+			Button curr = findViewById(v.getId());
 			switch (v.getId()) {
+				case R.id.plusBTN:
+				case R.id.minusBTN:
+				case R.id.multiplyBTN:
 				case R.id.divBTN:
-					((TextView) findViewById(R.id.dispTXT)).append(((Button)findViewById(R.id.divBTN)).getText());
+					((TextView) findViewById(R.id.dispTXT)).append(curr.getText());
 					break;
+			}
+		}
+	}
+
+	public enum Type {
+		NUM, VAR, OP, LEFT_PAREN, RIGHT_PAREN
+	}
+
+	public class Token {
+		private String value;
+		private Type type;
+
+		public Token(String value, Type type) {
+			this.value = value;
+			this.type = type;
+		}
+
+		public String getValue() {
+			return value;
+		}
+
+		public void setValue(String value) {
+			this.value = value;
+		}
+
+		public boolean isNum() {
+			if (this.type == NUM) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		public boolean isVar() {
+			if (this.type == VAR) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		public boolean isLeftParen() {
+			if (this.type == LEFT_PAREN) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		public boolean isRightParen() {
+			if (this.type == RIGHT_PAREN) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		public boolean isOperator() {
+			if (this.type == OP) {
+				return true;
+			} else {
+				return false;
 			}
 		}
 	}
