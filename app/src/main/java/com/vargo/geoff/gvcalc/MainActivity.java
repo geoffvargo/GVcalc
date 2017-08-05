2f1b5d590b4f1d2517e4fc78238cd6c5ab84f510
geoff <geoff.vargo@stonybrook.edu>
2017-08-05 02:46:24 -0400

    Modified existing button handlers to use the Token class. Created ArrayDeque<Token> tokens to act as a Tokens array for processing input, and adapted all button handlers to use it.

--- app/src/main/java/com/vargo/geoff/gvcalc/MainActivity.java
+++ app/src/main/java/com/vargo/geoff/gvcalc/MainActivity.java
@@ -7,8 +7,10 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayDeque;
 import java.util.Stack;
 
+import static com.vargo.geoff.gvcalc.MainActivity.Type.EMPTY;
 import static com.vargo.geoff.gvcalc.MainActivity.Type.LEFT_PAREN;
 import static com.vargo.geoff.gvcalc.MainActivity.Type.NUM;
 import static com.vargo.geoff.gvcalc.MainActivity.Type.OP;
@@ -17,10 +19,13 @@ import static com.vargo.geoff.gvcalc.MainActivity.Type.VAR;
 
 public class MainActivity extends Activity {
 
-	protected static Stack<Double> numStack = new Stack<>();
-	protected static Stack<Character> opStack = new Stack<>();
+	protected static Stack<Token> numStack = new Stack<>();
+	protected static Stack<Token> opStack = new Stack<>();
+
+	protected ArrayDeque<Token> tokens = new ArrayDeque<>();
 
 	protected String tempStr = "";
+	protected Token currTok = new Token();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,45 +35,61 @@ public class MainActivity extends Activity {
 	}
 
 	public void onNumClick(View view) {
+		if (!currTok.isNum() && !currTok.isEmpty()) {
+			tokens.push(currTok);
+			currTok = new Token(NUM);
+		} else if (currTok.isEmpty()) {
+			currTok.setType(NUM);
+		}
 		switch (view.getId()) {
 			case R.id.zeroBTN:
 				tempStr = tempStr.concat("0");
+				currTok.concat("0");
 				((TextView) findViewById(R.id.dispTXT)).append(String.valueOf(tempStr.charAt(tempStr.length())));
 				break;
 			case R.id.oneBTN:
 				tempStr = tempStr.concat("1");
+				currTok.concat("1");
 				((TextView) findViewById(R.id.dispTXT)).append(String.valueOf(tempStr.charAt(tempStr.length() - 1)));
 				break;
 			case R.id.twoBTN:
 				tempStr = tempStr.concat("2");
+				currTok.concat("2");
 				((TextView) findViewById(R.id.dispTXT)).append(String.valueOf(tempStr.charAt(tempStr.length() - 1)));
 				break;
 			case R.id.threeBTN:
 				tempStr = tempStr.concat("3");
+				currTok.concat("3");
 				((TextView) findViewById(R.id.dispTXT)).append(String.valueOf(tempStr.charAt(tempStr.length() - 1)));
 				break;
 			case R.id.fourBTN:
 				tempStr = tempStr.concat("4");
+				currTok.concat("4");
 				((TextView) findViewById(R.id.dispTXT)).append(String.valueOf(tempStr.charAt(tempStr.length() - 1)));
 				break;
 			case R.id.fiveBTN:
 				tempStr = tempStr.concat("5");
+				currTok.concat("5");
 				((TextView) findViewById(R.id.dispTXT)).append(String.valueOf(tempStr.charAt(tempStr.length() - 1)));
 				break;
 			case R.id.sixBTN:
 				tempStr = tempStr.concat("6");
+				currTok.concat("6");
 				((TextView) findViewById(R.id.dispTXT)).append(String.valueOf(tempStr.charAt(tempStr.length() - 1)));
 				break;
 			case R.id.sevenBTN:
 				tempStr = tempStr.concat("7");
+				currTok.concat("7");
 				((TextView) findViewById(R.id.dispTXT)).append(String.valueOf(tempStr.charAt(tempStr.length() - 1)));
 				break;
 			case R.id.eightBTN:
 				tempStr = tempStr.concat("8");
+				currTok.concat("8");
 				((TextView) findViewById(R.id.dispTXT)).append(String.valueOf(tempStr.charAt(tempStr.length() - 1)));
 				break;
 			case R.id.nineBTN:
 				tempStr = tempStr.concat("9");
+				currTok.concat("9");
 				((TextView) findViewById(R.id.dispTXT)).append(String.valueOf(tempStr.charAt(tempStr.length() - 1)));
 				break;
 		}
@@ -76,30 +97,88 @@ public class MainActivity extends Activity {
 
 	public void onOprClick(View v) {
 		if (!tempStr.isEmpty()) {
-			numStack.push(Double.valueOf(tempStr));
 			tempStr = "";
 			Button curr = findViewById(v.getId());
+			if (!currTok.isOperator() && !currTok.isEmpty()) {
+				tokens.push(currTok);
+				currTok = new Token(OP);
+			} else if (currTok.isEmpty()) {
+				currTok.setType(OP);
+			}
 			switch (v.getId()) {
 				case R.id.plusBTN:
+					((TextView) findViewById(R.id.dispTXT)).append(curr.getText());
+					currTok.concat(String.valueOf(curr.getText()));
+					break;
 				case R.id.minusBTN:
+					((TextView) findViewById(R.id.dispTXT)).append(curr.getText());
+					currTok.concat(String.valueOf(curr.getText()));
+					break;
 				case R.id.multiplyBTN:
+					((TextView) findViewById(R.id.dispTXT)).append(curr.getText());
+					currTok.concat(String.valueOf(curr.getText()));
+					break;
 				case R.id.divBTN:
 					((TextView) findViewById(R.id.dispTXT)).append(curr.getText());
+					currTok.concat(String.valueOf(curr.getText()));
 					break;
 			}
 		}
 	}
 
+	public void onLeftParenClick(View v) {
+		if (!currTok.isLeftParen() && !currTok.isEmpty()) {
+			tokens.push(currTok);
+			currTok = new Token(LEFT_PAREN);
+		} else if (currTok.isEmpty()) {
+			currTok.setType(LEFT_PAREN);
+		}
+		tempStr = tempStr.concat("(");
+		((TextView) findViewById(R.id.dispTXT)).append(String.valueOf(tempStr.charAt(tempStr.length() - 1)));
+		currTok.setValue("(");
+	}
+
+	public void onRightParenClick(View v) {
+		if (!currTok.isRightParen() && !currTok.isEmpty()) {
+			tokens.push(currTok);
+			currTok = new Token(RIGHT_PAREN);
+		} else if (currTok.isEmpty()) {
+			currTok.setType(RIGHT_PAREN);
+		}
+		tempStr = tempStr.concat(")");
+		((TextView) findViewById(R.id.dispTXT)).append(String.valueOf(tempStr.charAt(tempStr.length() - 1)));
+		currTok.setValue(")");
+	}
+
 	public enum Type {
-		NUM, VAR, OP, LEFT_PAREN, RIGHT_PAREN
+		NUM, VAR, OP, LEFT_PAREN, RIGHT_PAREN, EMPTY
 	}
 
 	public class Token {
-		private String value;
-		private Type type;
+		private String value = "";
+		private Type type = EMPTY;
+
+		public Token() {
+		}
+
+		public Token(Type type) {
+			this.type = type;
+		}
 
 		public Token(String value, Type type) {
 			this.value = value;
+			this.type = type;
+		}
+
+		public void concat(String str) {
+			this.value = this.value.concat(str);
+		}
+
+		public Type getType() {
+			return this.type;
+		}
+
+		public void setType(Type type) {
 			this.type = type;
 		}
 
@@ -149,6 +228,22 @@ public class MainActivity extends Activity {
 			} else {
 				return false;
 			}
+		}
+
+		public boolean isEmpty() {
+			if (this.type == EMPTY) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		@Override
+		public String toString() {
+			return "Token{" +
+					"value='" + value + '\'' +
+					", type=" + type +
+					'}';
 		}
 	}
 }
